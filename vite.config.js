b82f6ccb1f1c7d3a0492089a866af5a433c29b4a
timothymwangi-debug <timothy.mwangi@digitaldividedata.com>
2026-06-01 11:06:32 +0000

    domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://www.barakadigitalhub.com",
+      outDir: "dist",
+      generateRobotsTxt: true,
+      robots: [{ userAgent: "*", allow: "/" }],
+    }),
+  ],
   build: { outDir: "dist" },
   publicDir: "public",
 });
